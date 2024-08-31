0dcc33b512c06c9b8f51cc302db7a33f9ea0faca
cyc-samantha <yc-samantha@users.noreply.github.com>
2024-08-31 16:52:55 +0100

    build first Restapi connector

---
+++ RestAPIPractice/RestAPIPractice/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestAPIPractice.Model;
+
+namespace RestAPIPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private List<Category> categories = new List<Category>()
+        {
+            new Category() { Id = 0, name = "Appartment", description = "Hola"},
+            new Category() { Id = 1, name = "Appartment", description = "Hola"},
+        };
+
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return categories;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Category category)
+        {
+            categories.Add(category);
+        }
+
+        [HttpPut("id")]
+        public void Put(int id, [FromBody] Category category)
+        {
+            categories[id] = category;
+        }
+    };
+}
---
+++ RestAPIPractice/RestAPIPractice/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace RestAPIPractice.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+    }
+}
