b58cdece19b6569a1fe81c4a10fdb7723825faae
cyc-samantha <yc-samantha@users.noreply.github.com>
2024-09-01 12:40:09 +0100

    try get, post, delete, request

--- RestAPIPractice/RestAPIPractice/Model/Category.cs
+++ RestAPIPractice/RestAPIPractice/Model/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace RestAPIPractice.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestAPIPractice.Model
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Category name can't be null")]
         public string name { get; set; }
+        [Required(ErrorMessage = "Description can't be null")]
         public string description { get; set; }
         public string Url { get; set; }
     }
